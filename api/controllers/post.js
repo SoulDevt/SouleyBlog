43b362c53e96e9016999ee872cfb400b5f17ba33
SoulTheDevt <souleymane.gueye.pro@gmail.com>
2022-10-12 22:57:04 +0200

    fetch posts by categories

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -5,10 +5,9 @@ export const addPost = (req, res) => {
 }
 
 export const getPosts = (req, res) => {
-    const q = req.params.cat ?  "SELECT * from posts where cat = ?" : "SELECT * from posts"
-
+    const q = req.query.cat ?  "SELECT * from posts where cat=?" : "SELECT * from posts"
     //import db and launch request
-    db.query(q, [req.params.cat], (err,data) => {
+    db.query(q, [req.query.cat], (err,data) => {
         if(err) return res.send(err)
         return res.status(200).json(data);
     })
