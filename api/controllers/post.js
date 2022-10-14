1f4bc267f1e64e90af629459735513706bb07146
SoulTheDevt <souleymane.gueye.pro@gmail.com>
2022-10-15 00:25:07 +0200

    config single page and join table users and posts

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -14,7 +14,11 @@ export const getPosts = (req, res) => {
 }
 
 export const getPost = (req, res) => {
-    res.json("it works from controller");
+    const q = "SELECT `username`, `title`, `desc`, p.img, u.img AS userImg ,`cat`, `date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ?"
+    db.query(q, [req.params.id], (err,data) => {
+        if(err) return res.send(err)
+        return res.status(200).json(data[0]);
+    })
 }
 
 export const deletePost = (req, res) => {
